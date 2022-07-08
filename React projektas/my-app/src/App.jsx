e4e915f6358d0a00c36c12791ec91e81ea6e45b4
EdgarasKit <edgar.kitkevich@gmail.com>
2022-07-08 09:56:20 +0300

    importai routes

--- React projektas/my-app/src/App.jsx
+++ React projektas/my-app/src/App.jsx
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+
+import Add from "./components/Add/add";
+import Home from "./components/Home/home";
+import Login from "./components/Login/login";
+import Navi from "./components/Navi/navi";
+import Register from "./components/Register/register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navi />
+    <Routes>
+      <Route path='/add' element={<Add />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+    </Routes>
+    </>
   );
 }
 
