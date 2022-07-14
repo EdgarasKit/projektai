cab9435ad4bef1d786cc6df249dd086e4d36d746
EdgarasKit <edgar.kitkevich@gmail.com>
2022-07-14 11:51:40 +0300

    index.js

--- server/index.js
+++ server/index.js
@@ -1,2 +1,40 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const app = express();
+
+if (!config.get("jwtPrivateKey")) {
+  console.log("FATAL ERROR: JwtPrivateKey not defined");
+  process.exit(1);
+}
+
+let mongoDB = "mongodb://localhost:27017/forum";
+
+mongoose
+  .connect(mongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("could not connect to mongoDB"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("request successfully sent!");
+});
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
